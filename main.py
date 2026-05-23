bff596009dfc24e5bed9a783e7d243f0ce1389fb
IlyaM206 <ilyamaslov2309@gmail.com>
2026-05-23 21:36:25 +0300

    Добавление кнопок Играть и Выход

--- main.py
+++ main.py
@@ -25,3 +25,18 @@ class StartMenu:
             Image.open(self.IMAGE_BACKGROUND).resize((1280,720), Image.Resampling.LANCZOS)
         )
         self.canvas.create_image(0, 0, image=self.bg_img, anchor="nw")
+        # кнопка "Играть"
+        self.start_img = ImageTk.PhotoImage(Image.open("images/startbut.png").resize((500,200), Image.Resampling.LANCZOS))
+
+        btn = tk.Button(
+            self.canvas,
+            image=self.start_img,
+            command=self.start_game,
+            cursor="hand2"
+        )
+        btn.place(relx=0.5, rely=0.5, anchor="center")
+
+        # кнопка выхода
+        exit_btn = tk.Button(self.canvas, text="ВЫХОД", font=("Arial", 14), bg="#F44336", fg="white",
+                             command=root.quit, cursor="hand2", width=8)
+        exit_btn.place(x=self.w - 100, y=self.h - 50)

bf32de281d226066d2471f80c4849b3fcd402107
IlyaM206 <ilyamaslov2309@gmail.com>
2026-05-23 21:41:38 +0300

    Добавляю мяч и ракетки определенного размера, позиции ракеток

--- game.py
+++ game.py
@@ -14,3 +14,27 @@ class PongGame:
         # Создаем Canvas с черным фоном (на всякий случай)
         self.canvas = tk.Canvas(root, highlightthickness=0, bg='black')
         self.canvas.pack(fill="both", expand=True)
+
+        # Параметры игры (упрощенные)
+        self.pw, self.ph = 20, 120  # ракетка
+        self.ball_size = 20
+        self.margin = 40
+        self.speed = 8
+        self.win_score = 5
+
+        # Позиции игроков
+        self.player1_y = self.h // 2 - self.ph // 2
+        self.player2_y = self.h // 2 - self.ph // 2
+        self.score1 = self.score2 = 0
+        self.game_over = False
+
+        # Управление
+        self.keys = {}
+
+        # Мяч
+        self.ball = {
+            "x": self.w // 2,
+            "y": self.h // 2,
+            "dx": random.choice([-5, 5]),
+            "dy": random.uniform(-3, 3)
+        }
